174e1838ecd7d2409ffb1ae27cde117eb3e549b2
Teslacoin <teslacoinfoundation@gmail.com>
2018-03-04 16:50:36 -0500

    Update version.h

--- src/version.h
+++ src/version.h
@@ -47,8 +47,8 @@ static const int MEMPOOL_GD_VERSION = 60002;
 // Database format Versioning
 static const int DATABASE_VERSION = 63001;
 
-#define DISPLAY_VERSION_MAJOR       3
-#define DISPLAY_VERSION_MINOR       4
+#define DISPLAY_VERSION_MAJOR       4
+#define DISPLAY_VERSION_MINOR       0
 #define DISPLAY_VERSION_REVISION    0
 #define DISPLAY_VERSION_BUILD       0
 
