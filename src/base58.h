fc40fd03f2bdd5fced34bf7cd02f755b0a24778d
Teslacoin <teslacoinfoundation@gmail.com>
2018-06-29 23:21:36 -0500

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -276,7 +276,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 11,  // Teslacoin: address begin with 'C'
+        PUBKEY_ADDRESS = 11,  // Teslacoin: address begin with '5'
         SCRIPT_ADDRESS = 8, 
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
